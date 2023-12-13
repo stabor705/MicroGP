e60c9b804cb75802b852292ca385907e350c86a0
Stanisław Borowy <stabor@startmail.com>
2023-12-13 15:55:30 +0100

    add serialization/deserialization presentatino

--- src/main/java/xyz/stabor/microgp/Main.java
+++ src/main/java/xyz/stabor/microgp/Main.java
@@ -1,18 +1,42 @@
 package xyz.stabor.microgp;
 
+import org.apache.commons.lang3.SerializationUtils;
 import xyz.stabor.microgp.geneticast.*;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import xyz.stabor.microgp.geneticast.variables.Program;
 
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) {
+        String programName = "example.microgp";
+        String serializedName = "exampleast.bin";
+
         GeneticNode program = Program.generate(new GenerationContext(5));
-        CharStream inputStream = CharStreams.fromString(program.toString());
-        MicroGPLexer lexer = new MicroGPLexer(inputStream);
-        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        MicroGPParser parser = new MicroGPParser(tokenStream);
-        ParseTree parseTree = parser.program();
-        System.out.println(program);
+
+        try {
+            FileWriter fileWriter = new FileWriter(programName);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write(program.toString());
+            bufferedWriter.close();
+            fileWriter.close();
+
+            FileOutputStream fileOutputStream = new FileOutputStream(serializedName);
+            byte[] serialized = SerializationUtils.serialize(program);
+            fileOutputStream.write(serialized);
+
+            FileInputStream fileInputStream = new FileInputStream(serializedName);
+            byte[] input = new byte[serialized.length];
+            fileInputStream.read(input);
+            Program deserialized = SerializationUtils.deserialize(input);
+            System.out.println(deserialized);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
