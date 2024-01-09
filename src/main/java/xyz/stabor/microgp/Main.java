3abc93786dde502be2c5cbb50bec5408ca92ac1e
Stanisław Borowy <stabor@startmail.com>
2024-01-09 19:38:31 +0100

    asefe

--- src/main/java/xyz/stabor/microgp/Main.java
+++ src/main/java/xyz/stabor/microgp/Main.java
@@ -1,10 +1,15 @@
 package xyz.stabor.microgp;
 
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.apache.commons.lang3.SerializationUtils;
+import xyz.stabor.microgp.MicroGPLexer;
+import xyz.stabor.microgp.MicroGPParser;
 import xyz.stabor.microgp.geneticast.*;
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
 import xyz.stabor.microgp.geneticast.variables.Program;
+import xyz.stabor.microgp.interpreter.Interpreter;
 
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -13,6 +18,17 @@ import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args) {
+        String program = "asd = 1 + false;";
+        CharStream stream = CharStreams.fromString(program);
+        MicroGPLexer lexer = new MicroGPLexer(stream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+        MicroGPParser parser = new MicroGPParser(commonTokenStream);
+        MicroGPParser.ProgramContext ctx = parser.program();
+        Interpreter interpreter = new Interpreter();
+        System.out.println(interpreter.visitProgram(ctx));
+    }
+
+    static void serializeAndDeserialize() {
         String programName = "example.microgp";
         String serializedName = "exampleast.bin";
 
